915c9422ccfc80917213937ecc6ff016e26df7ed
slaypni <punigumi@gmail.com>
2013-02-02 16:21:54 +0900

    add LICENSE

--- setup.py
+++ setup.py
@@ -5,21 +5,24 @@ version = '0.1.0'
 
 setup(name='xml2data',
       version=version,
-      description="a html parser with json like css selector",
+      description="a library for converting xml into native data",
       long_description="""\
-json like parser""",
+xml2data is a library for converting xml into native data, according to css-like format.""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='css json html parser',
-      author='Kazuaki Tanida',
+      keywords='xml html native css selector convert parser',
+      author='slaypni',
       author_email='punigumi@gmail.com',
-      url='',
-      license='BSD',
+      url='https://github.com/slaypni/xml2data',
+      license='MIT',
       packages=find_packages(exclude=['xml2data', 'xml2data.testsuite']),
       test_suite='xml2data.testsuite.suite',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          'lxml',
+          'cssselect',
+          'chardet',
+          'minimock'
       ],
       entry_points="""
       # -*- Entry points: -*-
